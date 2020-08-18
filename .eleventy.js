fb78ed26baab4c1b9ce5253f0d25172bce266b64
Eli Morris-Heft <eli.morris.heft@gmail.com>
2020-08-18 12:42:54 -0500

    Only copy the fontawesome assets we need into _site

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,15 @@ module.exports = function(eleventyConfig) {
   // allow using YAML data files
   eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
 
-  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('assets/fonts');
+  eleventyConfig.addPassthroughCopy('assets/images');
+  eleventyConfig.addPassthroughCopy('assets/scripts');
+  eleventyConfig.addPassthroughCopy('assets/styles');
+
+  eleventyConfig.addPassthroughCopy('assets/uswds');
+  eleventyConfig.addPassthroughCopy('assets/fontawesome-pro-5.14.0-web/css/fontawesome.min.css');
+  eleventyConfig.addPassthroughCopy('assets/fontawesome-pro-5.14.0-web/css/solid.min.css');
+  eleventyConfig.addPassthroughCopy('assets/fontawesome-pro-5.14.0-web/webfonts/fa-solid-900.woff2');
 
   // create the markdownify filter
   // thanks to https://twitter.com/jeremenichelli/status/1206564493632983040
