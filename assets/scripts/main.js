ff0ac47b87aadfa7f59e56e0ce073a8113a8296b
Eli Morris-Heft <eli.morris.heft@gmail.com>
2020-08-20 00:27:37 -0500

    Add the first three episodes and a subscribe button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,8 +3,23 @@ function init() {
   document.querySelectorAll('article .expand button, article .collapse button')
     .forEach(button => button.addEventListener('click', function() { toggleEpisodeExpanded(this.closest('article')); }));
 
+  // add subscribe shroud event listeners
+  document.querySelectorAll('.subscribe-button')
+  .forEach(button => button.addEventListener('click', function() { SubscribeShroud.show(); }));
+
+  document.getElementById('subscribe-shroud')
+    .addEventListener('click', function(ev) {
+      if(ev.target === this) { SubscribeShroud.hide(); }
+    });
+
+  document.getElementById('subscribe-close')
+   .addEventListener('click', function(ev) { SubscribeShroud.hide(); });
+
+  // add podcast player event listeners
   document.querySelectorAll('.play-button')
     .forEach(button => button.addEventListener('click', function() { togglePlayButton(this); }));
+
+
 }
 
 function toggleEpisodeExpanded(episodeEl) {
